e634fe729b546cee58289467f5d1d9e5d1395ac0
brunosas88 <bruno.sas@live.com>
2023-03-21 15:17:38 -0300

    Adiciona botao cancelar

--- script/termos.js
+++ script/termos.js
@@ -1,14 +1,7 @@
 const checkboxTermos = document.querySelector('#checkbox-termos');
-const botaoTermos = document.querySelector('#botao-aceite-termos');
+const botaoTermosContinuar = document.querySelector('#botao-aceite-termos');
+const botaoTermosCancelar = document.querySelector('#botao-cancelar-termos');
 
 checkboxTermos.addEventListener('click', (evento) => {
-	console.log(checkboxTermos.checked);
-
-	// console.log(botaoTermos.getAttribute('disabled'));
-
-	// if (checkboxTermos.checked) {
-	// 	botaoTermos.disabled = true;
-	// }else
-
-	botaoTermos.disabled = checkboxTermos.checked ? false : true;
+	botaoTermosContinuar.disabled = checkboxTermos.checked ? false : true;
 });
