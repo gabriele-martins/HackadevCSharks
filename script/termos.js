d795562e3b694397e1bb6d923013ac181bbae784
brunosas88 <bruno.sas@live.com>
2023-03-21 16:06:25 -0300

    Adiciona funcao para reconhecer scroll do texto

--- script/termos.js
+++ script/termos.js
@@ -1,7 +1,25 @@
 const checkboxTermos = document.querySelector('#checkbox-termos');
 const botaoTermosContinuar = document.querySelector('#botao-aceite-termos');
 const botaoTermosCancelar = document.querySelector('#botao-cancelar-termos');
+const divTextoTermos = document.querySelector('#div-texto-termos');
+const pTextTermos = document.querySelector('#div-texto-termos p');
 
 checkboxTermos.addEventListener('click', (evento) => {
 	botaoTermosContinuar.disabled = checkboxTermos.checked ? false : true;
 });
+
+divTextoTermos.addEventListener('scroll', (evento) => {
+	console.log(verficaScrollNoFinalDoTexto(divTextoTermos));
+});
+
+function verficaScrollNoFinalDoTexto(elemento) {
+	let alturaTotalCaixaTexto = elemento.scrollHeight;
+	let posicaoAtual = elemento.scrollTop;
+	let compensacao = elemento.offsetHeight;
+
+	if (posicaoAtual > alturaTotalCaixaTexto - compensacao) {
+		return true;
+	} else {
+		return false;
+	}
+}
