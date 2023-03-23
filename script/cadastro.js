906d2631ad4a4beda39227733b28fb5db1129de3
Marco Sousa <marcosousacontato@gmail.com>
2023-03-23 12:34:52 -0300

    Implementando validação de form

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,16 +1,76 @@
-const form = document.querySelector("#form");
-let campos = document.querySelectorAll(".required");
-let divs = document.querySelectorAll(".div-required");
-const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+const formulario = document.querySelector("#cadastro-cliente");
+const campos = document.querySelectorAll(".required");
+const divs_input = document.querySelectorAll(".input");
+const divs = document.querySelectorAll(".div-required");
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const celularRegex = /^\(\d{2}\) \d{5}-\d{4}$/;
 
-const submitButton = document.querySelector("#login");
-const nomeInput = document.querySelector("#nome");
-const sobrenomeInput = document.querySelector("#sobrenome");
-const errorMessage = document.querySelector(".msg");
-//const cpfInput = document.querySelector("#cpf");
-//const emailInput = document.querySelector("#email");
-//const celularInput = document.querySelector("#celular");
-//const senhaInput = document.querySelector("#senha");
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    validarNome();
+    validarSobrenome();
+    validarCPF();
+    validarEmail();
+    validarCelular();
+    validarSenha();
+});
+
+function setError(index) {
+    divs_input[index].style.border = "2px solid #e63636";
+    divs[index].style.display = "block";
+}
+
+function removerError(index) {
+    divs_input[index].style.border = "";
+    divs[index].style.display = "none";
+}
+const validarNome = () => {
+    if (campos[0].value.length < 3) {
+        setError(0);
+    } else {
+        removerError(0);
+    }
+};
+
+const validarSobrenome = () => {
+    if (campos[1].value.length < 3) {
+        setError(1);
+    } else {
+        removerError(1);
+    }
+};
+
+const validarCPF = () => {
+    if (campos[2].value.length < 14) {
+        setError(2);
+    } else {
+        removerError(2);
+    }
+};
+
+const validarEmail = () => {
+    if (!emailRegex.test(campos[3].value)) {
+        setError(3);
+    } else {
+        removerError(3);
+    }
+};
+
+const validarCelular = () => {
+    if (!celularRegex.test(campos[4].value)) {
+        setError(4);
+    } else {
+        removerError(4);
+    }
+};
+
+const validarSenha = () => {
+    if (campos[5].value.length < 8) {
+        setError(5);
+    } else {
+        removerError(5);
+    }
+};
 
 function mascaras() {
     mascaraCPF();
