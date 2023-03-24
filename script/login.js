705862a1c75a049ccd51d0b5677761049f5e948a
Gabriele Martins <gabriele.martinsp@gmail.com>
2023-03-23 21:21:36 -0300

    altera README  e adiciona função no botão de login

--- script/login.js
+++ script/login.js
@@ -1,23 +1,17 @@
-// const inputNome = document.querySelector("#cpf");
-// const inputSenha = document.querySelector("#senha");
-// const botaoEntrar = document.querySelector(".div-form-item button");
+const inputNome = document.querySelector("#cpf");
+const inputSenha = document.querySelector("#senha");
+const botaoEntrar = document.querySelector(".div-form-item button");
 
-// inputNome.addEventListener("change", (evento) => {
-//   validaInput(inputNome, inputSenha);
+inputNome.addEventListener("input", (evento) => {
+  validaInput(inputNome, inputSenha);
+});
 
-//   console.log(inputNome.value.length);
-//   console.log(inputSenha.value.length);
-// });
+inputSenha.addEventListener("input", (evento) => {
+  validaInput(inputNome, inputSenha);
+});
 
-// inputSenha.addEventListener("change", (evento) => {
-//   validaInput(inputNome, inputSenha);
-
-//   console.log(inputNome.value.length);
-//   console.log(inputSenha.value.length);
-// });
-
-// function validaInput(inputNome, inputSenha) {
-//   botaoEntrar.disabled = !(
-//     inputNome.value.length !== 0 && inputSenha.value.length !== 0
-//   );
-// }
+function validaInput(inputNome, inputSenha) {
+  botaoEntrar.disabled = !(
+    inputNome.value.length !== 0 && inputSenha.value.length !== 0
+  );
+}
