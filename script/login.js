19dcd832a24fcbe3de5af37a67e926dc7da84fdd
Gabriele Martins <gabriele.martinsp@gmail.com>
2023-03-23 21:10:49 -0300

    ultimas alterações de estilo

--- script/login.js
+++ script/login.js
@@ -1,8 +1,23 @@
-const inputNome = document.querySelector("#cpf");
-const inputSenha = document.querySelector("#senha");
-const botaoEntrar = document.querySelector(".div-form-item button");
-const formularioLogin = document.querySelector("#login-form");
+// const inputNome = document.querySelector("#cpf");
+// const inputSenha = document.querySelector("#senha");
+// const botaoEntrar = document.querySelector(".div-form-item button");
 
-formularioLogin.addEventListener("submit", (e) => {
-  console.log(inputNome.value);
-});
+// inputNome.addEventListener("change", (evento) => {
+//   validaInput(inputNome, inputSenha);
+
+//   console.log(inputNome.value.length);
+//   console.log(inputSenha.value.length);
+// });
+
+// inputSenha.addEventListener("change", (evento) => {
+//   validaInput(inputNome, inputSenha);
+
+//   console.log(inputNome.value.length);
+//   console.log(inputSenha.value.length);
+// });
+
+// function validaInput(inputNome, inputSenha) {
+//   botaoEntrar.disabled = !(
+//     inputNome.value.length !== 0 && inputSenha.value.length !== 0
+//   );
+// }
