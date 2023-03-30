7ebc0d37c46ccd6b0e3609a1f1cf1f62c99e1b7d
brunosas88 <bruno.sas@live.com>
2023-03-30 16:48:22 -0300

    Adiciona estrutura inicial para page Termos

--- SharksBank/src/App.jsx
+++ SharksBank/src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
+import { Termos } from "./pages/Termos/index";
 
 export function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+
+    <Termos/>
+
+  </div>;
 }
