1bf3de50195531a9f9795d822a2a7a5141d3d6bb
Gabriele Martins <gabriele.martinsp@gmail.com>
2023-04-01 14:59:02 -0300

    altera nome de paginas e adiciona global.css

--- SharksBank/src/components/Conteudo/index.jsx
+++ SharksBank/src/components/Conteudo/index.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from "react";
 
-import styles from "./style.module.css"
+import styles from "./index.module.css";
 
-export function Conteudo({titulo, subTitulo, descricao, children}) { 
-    return(
-        <div className={styles.container}>
-            <div className={styles.containerText}>
-                <h2>{titulo}</h2>
-                <span className={styles.subTitulo}>{subTitulo}</span>
-                <p>{descricao}</p>
-            </div>
-            <div className={styles.containerConteudoExterno}>{children}</div>
-            
-        </div>
-    )
+export function Conteudo({ titulo, subTitulo, descricao, children }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerText}>
+        <h2>{titulo}</h2>
+        <span className={styles.subTitulo}>{subTitulo}</span>
+        <p>{descricao}</p>
+      </div>
+      <div className={styles.containerConteudoExterno}>{children}</div>
+    </div>
+  );
 }
