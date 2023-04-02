fe3fe7740bbbf420fc15e594904c57f71d4909e8
keilertrindade <keiler.trindade@hotmail.com>
2023-04-02 16:08:48 -0300

    Adiciona página Home

--- SharksBank/src/App.jsx
+++ SharksBank/src/App.jsx
@@ -1,30 +1,12 @@
 import React from "react";
-import { Botao } from "./components/Botao";
-import { Header } from "./components/Header";
-import { Menu } from "./components/Menu";
-import { Conteudo } from "./components/Conteudo";
-import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home";
 
 import "./App.css";
 
 export function App() {
   return (
     <div className="app">
-      <Header
-        menu={<Menu></Menu>}
-        botoes={
-          <div className="teste">
-            <Botao desabilitado={false} nome={"Abra sua conta"} />
-            <Botao desabilitado={false} nome={"Acessar"} />
-          </div>
-        }
-      />
-      <Conteudo
-        titulo={"Home"}
-        subTitulo={"Testando"}
-        descricao={"Testando mais ainda"}
-      ></Conteudo>
-      <Footer />
-    </div>
+      <Home/>
+    </div> 
   );
 }
