2c07e39dce6e2db7c22d5a43c667d749968c4fd3
Tais Saito <saitotais@gmail.com>
2023-04-03 15:34:03 -0300

    Pagina recuperacao de senha sem CSS

--- SharksBank/src/App.jsx
+++ SharksBank/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Home } from "./pages/Home";
+import { RecuperaSenha } from "./pages/RecuperaSenha";
 
 import "./App.css";
 
 export function App() {
   return (
     <div className="app">
-      <Home/>
+      <RecuperaSenha/>
     </div> 
   );
 }
