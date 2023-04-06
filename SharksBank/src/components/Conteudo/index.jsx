64acf673f3e51356ae3ffd6ac48ccf0037f81098
Raylson Moreira <raylson.lima@outlook.com>
2023-04-06 12:08:37 -0300

    adiciona quarta div ao Conteudo
    adicionado uma div do lado dos textos abaixo da descrição

--- SharksBank/src/components/Conteudo/index.jsx
+++ SharksBank/src/components/Conteudo/index.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 import styles from "./index.module.css";
 
-export function Conteudo({ titulo, subTitulo, descricao, children }) {
+export function Conteudo({ titulo, subTitulo, descricao, children, descricaoContinuacao }) {
   return (
     <div className={styles.conteudo}>
       <div className={styles.containerText}>
         <h2>{titulo}</h2>
         <span className={styles.subTitulo}>{subTitulo}</span>
         <p>{descricao}</p>
+        <p>{descricaoContinuacao}</p>
       </div>
       <div className={styles.containerConteudoExterno}>{children}</div>
     </div>
