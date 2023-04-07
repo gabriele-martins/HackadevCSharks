57936379af318299297db7b2649670d65e8d0766
brunosas88 <bruno.sas@live.com>
2023-04-06 21:30:07 -0300

    Adiciona componente CampoInput
    Componente geral para criacao de inputs

---
+++ SharksBank/src/components/Inputs/index.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import styles from './index.module.css';
+
+export function CampoInput({
+	nome,
+	idNome,
+	campoReferencia,
+	labelNome,
+	register,
+	errors,
+	obrigatorio,
+	padrao,
+	tamanhoMax,
+	tamanhoMin,
+	desabilitar,
+}) {
+	return (
+		<div className={styles.containerInput}>
+			<label htmlFor={idNome}>{labelNome}</label>
+			<input
+				name={nome}
+				disabled={desabilitar}
+				maxLength={tamanhoMax}
+				id={idNome}
+				{...register(campoReferencia, {
+					required: obrigatorio,
+					pattern: padrao,
+					minLength: tamanhoMin,
+				})}
+			/>
+			{obrigatorio && errors[campoReferencia]?.type === 'required' && (
+				<span> Campo obrigatório! </span>
+			)}
+			{errors[campoReferencia]?.type === 'pattern' && (
+				<span> {labelNome} inválido(a) </span>
+			)}
+			{errors[campoReferencia]?.type === 'minLength' && (
+				<span> Tamanho mínimo: {tamanhoMin} </span>
+			)}
+		</div>
+	);
+}
