9a15762e386d4767fe00178920494db760a18a33
Raylson Moreira <raylson.lima@outlook.com>
2023-04-07 10:49:23 -0300

    Atualizando componente login
    Aguardando o router

--- SharksBank/src/pages/Login/index.jsx
+++ SharksBank/src/pages/Login/index.jsx
@@ -1,30 +1,39 @@
+import { useForm } from "react-hook-form";
 import React, { useState } from "react";
 import { Conteudo } from "../../components/Conteudo";
+import { CampoInput } from "../../components/Inputs";
 import { Botao } from "../../components/Botao";
 import styles from "./style.module.css";
 export function Login() {
-  const [cpfValido, setCpfValido] = useState(false);
-  const [senhaValida, setSenhaValida] = useState(false);
-  const [cpfDados, setCpfDados] = useState("");
-  const [senhaDados, setSenhaDados] = useState("");
+  // const [cpfValido, setCpfValido] = useState(false);
+  // const [senhaValida, setSenhaValida] = useState(false);
+  // const [cpfDados, setCpfDados] = useState("");
+  // const [senhaDados, setSenhaDados] = useState("");
 
-  function handleCpfDados(evento) {
-    setCpfDados(evento.target.value);
-    if (cpfDados.trim() !== "") setCpfValido(true);
-    else setCpfValido(false);
-    console.log("cpf: " + cpfValido);
-  }
+  // function handleCpfDados(evento) {
+  //   setCpfDados(evento.target.value);
+  //   if (cpfDados.trim() !== "") setCpfValido(true);
+  //   else setCpfValido(false);
+  //   console.log("cpf: " + cpfValido);
+  // }
 
-  function handleSenhaDados(evento) {
-    setSenhaDados(evento.target.value);
-    if (senhaDados.trim() !== "") setSenhaValida(true);
-    else setSenhaValida(false);
-    console.log("senha: " + senhaValida);
-  }
+  // function handleSenhaDados(evento) {
+  //   setSenhaDados(evento.target.value);
+  //   if (senhaDados.trim() !== "") setSenhaValida(true);
+  //   else setSenhaValida(false);
+  //   console.log("senha: " + senhaValida);
+  // }
 
-  function handleHabilitaBotao() {
-    return senhaValida && cpfValido;
-  }
+  // function handleHabilitaBotao() {
+  //   return senhaValida && cpfValido;
+  // }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onChange" });
+
+  const onSubmit = (data) => console.log(data);
 
   return (
     <>
@@ -39,8 +48,8 @@ export function Login() {
         }
       >
         <div id={styles["login-form-div"]}>
-          <form action="" id={styles["login-form"]}>
-            <div className={styles.divFormItem}>
+          <form onSubmit={handleSubmit(onSubmit)} id={styles["login-form"]}>
+            {/* <div className={styles.divFormItem}>
               <input
                 type="text"
                 name="cpf"
@@ -61,11 +70,36 @@ export function Login() {
                 onBlur={handleSenhaDados}
                 placeholder="Digite sua senha"
               />
+            </div> */}
+            <div className={styles.divFormItem}>
+              <CampoInput
+                campoReferencia={"cpf"}
+                placeholder={"Digite seu cpf"}
+                obrigatorio={true}
+                padrao={/^[0-9]*$/}
+                tamanhoMax={11}
+                tamanhoMin={11}
+                register={register}
+                errors={errors}
+              />
+            </div>
+            <div className={styles.divFormItem}>
+              <CampoInput
+                placeholder={"Digite sua senha"}
+                tipo={"password"}
+                campoReferencia={"senha"}
+                tamanhoMin={6}
+                tamanhoMax={6}
+                obrigatorio={true}
+                register={register}
+                errors={errors}
+              />
             </div>
             <div className={styles.divFormItem}>
               <Botao
                 nome={"Entrar"}
-                desabilitado={!(cpfValido && senhaValida)}
+                type="submit"
+                //desabilitado={!(cpfValido && senhaValida)}
               ></Botao>
             </div>
             <div className={styles.divFormItem}>
