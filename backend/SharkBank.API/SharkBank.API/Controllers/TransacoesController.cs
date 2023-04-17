fbfcdc7d29225d364e51317e9394f2c46b0dadf3
Marco Sousa <marcosousacontato@gmail.com>
2023-04-17 16:58:13 -0300

    Adicionando transacao controller, post and get

---
+++ backend/SharkBank.API/SharkBank.API/Controllers/TransacoesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using SharkBank.API.Data.Context;
+using SharkBank.API.Domain.DTO;
+using SharkBank.API.Domain.Models;
+
+namespace SharkBank.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransacoesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public TransacoesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Transacao>>> Get(int contaId)
+        {
+            var conta = await _context.Contas.FirstOrDefaultAsync(c => c.Id == contaId);
+            if (conta == null)
+            {
+                return NotFound("Conta não existe");
+            }
+            var transacoes = await _context.Transacoes
+                .Where(t => t.ContaId == contaId)
+                .ToListAsync(); 
+            return transacoes;
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<List<Transacao>>> Post(CriarTransferenciaDTO requisicao)
+        {
+            var conta = await _context.Contas.FindAsync(requisicao.ContaId);
+            if (conta == null)
+            {
+                return NotFound("Usuário não encontrado"); 
+            }
+
+            var novaTransacao = new Transacao
+            {
+                Nome = requisicao.Nome,
+                Data = requisicao.Data,
+                Valor = requisicao.Valor,
+                Tipo = requisicao.Tipo,
+                IsEnviada = requisicao.isEnviada,
+                Conta = conta
+            }; 
+            _context.Transacoes.Add(novaTransacao);
+            await _context.SaveChangesAsync();
+            return await Get(novaTransacao.ContaId);
+        }
+    }
+}
---
+++ backend/SharkBank.API/SharkBank.API/Domain/DTO/CriarTransferenciaDTO.cs
@@ -0,0 +1,14 @@
+﻿using SharkBank.API.Domain.Models.Enums;
+
+namespace SharkBank.API.Domain.DTO
+{
+    public class CriarTransferenciaDTO
+    {
+        public string Nome { get; set; } = "Text3";
+        public DateTime Data { get; set; } = DateTime.Now;
+        public double Valor { get; set; } = 200.00;
+        public TipoTransacao Tipo { get; set; } = TipoTransacao.TED;
+        public bool isEnviada { get; set; } = true;
+        public int ContaId { get; set; }
+    }
+}
